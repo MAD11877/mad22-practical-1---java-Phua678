8caed26bf4b2fdca44db5c397228e53ed4ec2d33
Phua678 <94032313+Phua678@users.noreply.github.com>
2022-04-21 09:35:25 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -4,12 +4,12 @@ public class Question2
 {
   public static void main(String[] args)
   { 
-    System.out.print("Enter your height in metres:");
+    //System.out.print("Enter your height in metres:");
     Scanner in= new Scanner(System.in); 
     double height = in.nextDouble();
-    System.out.print("Enter your weight in kg: "); 
+    //System.out.print("Enter your weight in kg: "); 
     double weight = in.nextInt();
     double bmi = weight/((height)*(height));
-    System.out.println("Your BMI is " + bmi);  
+    //System.out.println("Your BMI is " + bmi);  
   }
 }
