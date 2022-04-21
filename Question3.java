da450ccdb389b7489b1989c6cf1de981c3e4c508
Phua678 <94032313+Phua678@users.noreply.github.com>
2022-04-21 09:52:29 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -4,10 +4,11 @@ public class Question3
 {
   public static void main(String[] args)
   {
-    System.out.print("Input an integer:");
+    //System.out.print("Input an integer:");
     Scanner in = new Scanner(System.in);
     int number = in.nextInt();
     int answer = number*number; 
-    System.out.println("Your answer is " + answer);  
+    //System.out.println("Your answer is " + answer);  
+    System.out.println(answer);
   }
-}
+};
